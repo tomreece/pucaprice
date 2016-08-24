31627384cb8d7a05245149468f05e4a150097499
Tom Reece <tomreece@gmail.com>
2016-08-23 22:55:03 -0500

    Scrape set, haves, and wants

--- scraper.py
+++ scraper.py
@@ -15,9 +15,16 @@ def scrape():
             soup = BeautifulSoup(r.text, "html.parser")
 
             card_name = soup.find("h1", class_="title").text
+            set_name = soup.find("div", class_="type").find("a").find("span").text
             prices = soup.find_all("div", class_="price small")
+            # PucaTrade has uses the class "have" for both have and want counts
+            have_wants = soup.find_all("div", class_="have")
             normal_price = prices[0].text.replace(",", "")
+            normal_haves = have_wants[0].text.replace(" HAVES", "")
+            normal_wants = have_wants[1].text.replace(" WANTS", "")
             foil_price = prices[1].text.replace(",", "")
+            foil_haves = have_wants[2].text.replace(" HAVES", "")
+            foil_wants = have_wants[3].text.replace(" WANTS", "")
 
             if normal_price == "N/A":
                 normal_price = None
@@ -27,15 +34,19 @@ def scrape():
 
             card = {
                 "card_name": card_name,
+                "set_name": set_name,
                 "pucatrade_id": pucatrade_id,
                 "normal_price": normal_price,
-                "foil_price": foil_price
+                "normal_haves": normal_haves,
+                "normal_wants": normal_wants,
+                "foil_price": foil_price,
+                "foil_haves": foil_haves,
+                "foil_wants": foil_wants
             }
 
             r2 = requests.post("http://pucaprice.herokuapp.com/scraper/add", json=card)
 
             print(card)
-            print(r2.status_code)
         except Exception:
             pass
 
--- web.py
+++ web.py
@@ -18,6 +18,7 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     pucatrade_id = db.Column(db.Integer)
     name = db.Column(db.String)
+    set_name = db.Column(db.String)
 
 class Price(db.Model):
     __tablename__ = "prices"
@@ -25,7 +26,11 @@ class Price(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     card_id = db.Column(db.Integer, db.ForeignKey("cards.id"))
     normal = db.Column(db.Integer)
+    normal_haves = db.Column(db.Integer)
+    normal_wants = db.Column(db.Integer)
     foil = db.Column(db.Integer)
+    foil_haves = db.Column(db.Integer)
+    foil_wants = db.Column(db.Integer)
     created = db.Column(db.DateTime(timezone=True), default=datetime.datetime.utcnow)
 
     card = db.relationship("Card", primaryjoin=(card_id == Card.id), backref=db.backref("card", lazy="dynamic"))
@@ -46,12 +51,17 @@ def scraper_add():
         card = Card()
         card.pucatrade_id = data["pucatrade_id"]
         card.name = data["card_name"]
+        card.set_name = data["set_name"]
         db.session.add(card)
         db.session.commit()
     price = Price()
     price.card_id = card.id
     price.normal = data["normal_price"]
+    price.normal_haves = data["normal_haves"]
+    price.normal_wants = data["normal_wants"]
     price.foil = data["foil_price"]
+    price.foil_haves = data["foil_haves"]
+    price.foil_wants = data["foil_wants"]
     db.session.add(price)
     db.session.commit()
     return jsonify({ "success": True })
