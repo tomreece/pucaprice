399068a3b0f7a34ed63494103d857cf6a3858386
Tom Reece <tomreece@gmail.com>
2016-08-23 22:17:07 -0500

    Continue on Exceptions, there are gaps in pucatrade card IDs

--- scraper.py
+++ scraper.py
@@ -10,31 +10,34 @@ sched = BlockingScheduler()
 @sched.scheduled_job("interval", days=1)
 def scrape():
     for pucatrade_id in range(1, RANGE + 1):
-        r = requests.get("https://pucatrade.com/cards/show/{}".format(pucatrade_id))
-        soup = BeautifulSoup(r.text, "html.parser")
+        try:
+            r = requests.get("https://pucatrade.com/cards/show/{}".format(pucatrade_id))
+            soup = BeautifulSoup(r.text, "html.parser")
 
-        card_name = soup.find("h1", class_="title").text
-        prices = soup.find_all("div", class_="price small")
-        normal_price = prices[0].text.replace(",", "")
-        foil_price = prices[1].text.replace(",", "")
+            card_name = soup.find("h1", class_="title").text
+            prices = soup.find_all("div", class_="price small")
+            normal_price = prices[0].text.replace(",", "")
+            foil_price = prices[1].text.replace(",", "")
 
-        if normal_price == "N/A":
-            normal_price = None
+            if normal_price == "N/A":
+                normal_price = None
 
-        if foil_price == "N/A":
-            foil_price = None
+            if foil_price == "N/A":
+                foil_price = None
 
-        card = {
-            "card_name": card_name,
-            "pucatrade_id": pucatrade_id,
-            "normal_price": normal_price,
-            "foil_price": foil_price
-        }
+            card = {
+                "card_name": card_name,
+                "pucatrade_id": pucatrade_id,
+                "normal_price": normal_price,
+                "foil_price": foil_price
+            }
 
-        r2 = requests.post("http://pucaprice.herokuapp.com/scraper/add", json=card)
+            r2 = requests.post("http://pucaprice.herokuapp.com/scraper/add", json=card)
 
-        print(card)
-        print(r2.status_code)
+            print(card)
+            print(r2.status_code)
+        except Exception:
+            pass
 
 # Scrape on startup
 scrape()
