06a720de9164f55137939e65444253536611b9a5
Tom Reece <tomreece@gmail.com>
2016-08-25 01:35:56 -0500

    Temporarily hide prices, no clue whats up

--- web.py
+++ web.py
@@ -77,23 +77,23 @@ def search(query):
 #
 
 def card_to_dict(card):
-    price = Price.query.filter(Price.card_id == card.id).order_by(Price.id.desc()).first()
+    price = Price.query.filter_by(card_id=card.id).order_by(Price.id.desc()).first()
     return {
         "name": card.name,
         "set_name": card.set_name,
-        "url": "https://pucatrade.com/cards/show/{}".format(card.pucatrade_id),
-        "prices": {
-            "normal": price.normal,
-            "foil": price.foil
-        },
-        "haves": {
-            "normal": price.normal_haves,
-            "foil": price.foil_haves
-        },
-        "wants": {
-            "normal": price.normal_wants,
-            "foil": price.foil_wants
-        }
+        "url": "https://pucatrade.com/cards/show/{}".format(card.pucatrade_id)
+        # "prices": {
+        #     "normal": price.normal,
+        #     "foil": price.foil
+        # },
+        # "haves": {
+        #     "normal": price.normal_haves,
+        #     "foil": price.foil_haves
+        # },
+        # "wants": {
+        #     "normal": price.normal_wants,
+        #     "foil": price.foil_wants
+        # }
     }
 
 #
